0b66bad71546d99432591bd798a5b240478c5611
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-13 20:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstChar(text) {
+	
   // your code here
+	text=text.trim();
+	if(text.length==0)return '';
+	return text[0];
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
+
 alert(firstChar(text));
